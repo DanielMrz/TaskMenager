6371967245b7698d9a9a398f9e11f6cca69d4bba
Daniel Mrzygłód <daniel.mrz19@gmail.com>
2023-01-14 16:17:12 +0100

    Model, controller and simple get data

---
+++ TaskMenager/Models/TaskModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskMenager.Models
+{
+    public class TaskModel
+    {
+        public int TaskId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Done { get; set; }
+
+    }
+}
